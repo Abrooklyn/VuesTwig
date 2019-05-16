37998526bb4e527937dac4cd73a03bdf81e6f181
Angelique Vannespenne <angeliquevannespenne@hotmail.fr>
2019-05-16 11:34:58 +0200

    :fire:

--- public/index.php
+++ public/index.php
@@ -15,5 +15,5 @@ $twig = new Twig_Environment($load);
 
 $products = ['product1', 'product2', 'product3', 'product4', 'product5'];
 
-echo $twig->render("hello.html.twig");
+echo $twig->render("base.html.twig");
 echo $twig->render("index.html.twig",[ 'array' => $products]);
